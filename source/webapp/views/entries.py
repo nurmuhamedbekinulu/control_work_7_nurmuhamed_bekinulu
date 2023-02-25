1cbb20a60d4fad14f2e183b9e5da5d892d369665
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-02-25 16:47:41 +0600

    реализация проверки

--- source/webapp/views/entries.py
+++ source/webapp/views/entries.py
@@ -5,6 +5,8 @@ from static.classes.static import Static
 
 
 def add_view(request: WSGIRequest):
+    errors = {}
+
     if request.method == "GET":
         return render(request, 'entry_create.html', context={'choices': Static.choices})
 
@@ -13,19 +15,51 @@ def add_view(request: WSGIRequest):
         'author_mailbox': request.POST.get('author_mailbox'),
         'text': request.POST.get('text')
     }
-    entry = Entry.objects.create(**entry_data)
-    return redirect('index')
+    if not request.POST.get('author_name'):
+        errors['author_name'] = 'Поле с именем обязательно к заполнению'
+
+    if not request.POST.get('author_mailbox'):
+        errors['author_mailbox'] = 'Поле с почтой обязательно к заполнению'
+
+    if not request.POST.get('text'):
+        errors['text'] = 'Поле с текстом записи обязательно к заполнению'
+
+    if errors:
+        return render(request, 'entry_create.html',
+                      context={
+                          'errors': errors
+                      })
+    else:
+        entry = Entry.objects.create(**entry_data)
+        return redirect('index')
 
 
 def update_view(request, pk):
+    errors = {}
+
     entry = get_object_or_404(Entry, pk=pk)
     if request.method == "POST":
+        if not request.POST.get('author_name'):
+            errors['author_name'] = 'Поле с именем обязательно к заполнению'
+
+        if not request.POST.get('author_mailbox'):
+            errors['author_mailbox'] = 'Поле с почтой обязательно к заполнению'
+
+        if not request.POST.get('author_name'):
+            errors['text'] = 'Поле с текстом записи обязательно к заполнению'
+
         entry.author_name = request.POST.get('author_name')
         entry.author_mailbox = request.POST.get('author_mailbox')
         entry.text = request.POST.get('text')
+        if errors:
+            return render(request, 'entry_update.html',
+                          context={
+                              'entry': entry,
+                              'errors': errors
+                          })
         entry.save()
         return redirect('index')
-    return render(request, 'entry_update.html', context={'entry': entry, 'choices': Static.choices})
+    return render(request, 'entry_update.html', context={'entry': entry})
 
 
 def delete_view(request, pk):
